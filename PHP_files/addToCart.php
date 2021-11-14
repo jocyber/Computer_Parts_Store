17166c5453ba24c56ca9dad2e8175a7ea7e8d636
jocyber <jordaneharman@protonmail.com>
2021-11-14 10:47:08 -0500

    added login and sign up and made some modifications to the user id used in the database

--- PHP_files/addToCart.php
+++ PHP_files/addToCart.php
@@ -14,9 +14,14 @@
     $price = $row['Price'];
     $dir = $row['img_dir'];
     $type = $row['type'];
-    $id = session_id();
 
-    $result = mysqli_query($conn, "insert into cart (Name, Price, img_dir, type, UserID) values ('$name', $price, '$dir', '$type', '$id');");
+    $name = $_SESSION["uname"];
+
+    $select = mysqli_query($conn, "select ID from users where Username='$name'");
+    $row = mysqli_fetch_assoc($select);
+    $id = $row["ID"];
+
+    $result = mysqli_query($conn, "insert into cart (Name, Price, img_dir, type, UserID) values ('$name', $price, '$dir', '$type', $id);");
     
     mysqli_close($conn);
 ?>
--- PHP_files/login.php
+++ PHP_files/login.php
@@ -13,17 +13,20 @@
     $username = $_POST["uname"];
     $password = $_POST["psw"];
     session_start();
-    $id = session_id();
 
-    $query = mysqli_query($conn, "insert into users (ID, Username, password) values ('$id', '$username', '$password')");
+    $login_attempt = mysqli_query($conn, "select * from users where Username='$username'");
+    $result = mysqli_fetch_assoc($login_attempt);
 
-    if(isset($_POST["remember"])) {
-        setcookie("uname", $username, time() + 31536000);
+    if($result == 0) {
+        $query = mysqli_query($conn, "insert into users (Username, password) values ('$username', '$password')");
+
+        if(isset($_POST["remember"])) {
+            setcookie("uname", $username, time() + 31536000);
+        }
     }
 
     $_SESSION["uname"] = $username;
     $_SESSION["passw"] = $password;
-
     mysqli_close($conn);
     header("Location: ../html_files/index.php");
 ?>
